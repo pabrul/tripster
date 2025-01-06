eab37152f82df0537bd1c0f401fd7aebd50da98a
pabrul <pablohrbatista@gmail.com>
2025-01-06 00:55:05 -0300

    chore: update project configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
-  css: ['~/assets/css/tailwind.css'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@pinia/nuxt"],
+  css: ["~/assets/css/tailwind.css"],
   tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.js'
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
   },
-})
+});
