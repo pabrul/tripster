f59ecf518c3c2010ee64d01640bbfa17ad150a16
pabrul <pablohrbatista@gmail.com>
2025-01-06 00:55:49 -0300

    feat: implement alert system in root component

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
+// app.vue
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <Alert />
   </div>
 </template>
+
+<script setup>
+import Alert from "@/components/base/Alert.vue";
+</script>
